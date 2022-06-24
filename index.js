162e91897ad8e429df928ab52aae94059ac9615b
unknown <coder.rakibul@gmail.com>
2022-06-24 16:28:19 +0600

    payment api success

--- index.js
+++ index.js
@@ -103,6 +103,47 @@ async function run() {
 
         app.post('/checkout', async (req, res) => {
             console.log(req.body)
+
+            let error, status
+            try {
+                const { product, token } = req.body;
+                const customer = await stripe.customers.create({
+                    email: token.email,
+                    source: token.id
+                });
+
+                const key = uuid();
+                const charge = await stripe.charges.create(
+                    {
+                        amount: product.price * 100,
+                        currency: "usd",
+                        customer: customer.id,
+                        receipt_email: token.email,
+                        description: `Purchased the ${product.name}`,
+                        shipping: {
+                            name: token.card.name,
+                            address: {
+                                line1: token.card.address_line1,
+                                line2: token.card.address_line2,
+                                city: token.card.address_city,
+                                country: token.card.address_country,
+                                postal_code: token.card.address_zip,
+                            },
+                        },
+                    },
+                    {
+                        key,
+                    }
+                );
+                console.log("Charge:", { charge });
+                status = "success";
+
+            }
+            catch (error) {
+                console.log(error);
+                status = "failure";
+            }
+            res.json({ error, status });
         })
 
 
