9a974c3e71bbd72864ffd29110379bd92c475302
unknown <coder.rakibul@gmail.com>
2022-06-26 02:49:41 +0600

    payment gateway ready

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const bodyparser = require('body-parser');
-const stripe = require('stripe')('sk_test_51L0gwOCHgcol8ks1CaDSZ4HEn1uf4aiMvMel7eGpJtR4738nIZpfUouwuXC8GRkUVUAMRvRtgbhLI5LdnZ9aJ0vB00ZdHKwZoZ');
-const uuid = require('uuid').v4
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyparser.urlencoded({ extended: false }));
-app.use(bodyparser.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nycpx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -26,6 +22,18 @@ async function run() {
         const cartCollection = client.db('panda_commerce').collection('carts');
         const orderCollection = client.db('panda_commerce').collection('orders');
         const paymentCollection = client.db('panda_commerce').collection('payments');
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const product = req.body;
+            const price = product.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
 
         app.get('/item', async (req, res) => {
             const query = {};
@@ -106,9 +114,25 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const order = await orderCollection.findOne(query);
             res.send(order);
+        });
+
+        app.patch('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+
+            const result = await paymentCollection.insertOne(payment);
+            const updatedOrder = await orderCollection.updateOne(filter, updatedDoc);
+            res.send(updatedOrder);
         })
 
-       
+
 
         app.delete('/order/:id', async (req, res) => {
             const id = req.params.id;
